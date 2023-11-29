92a12a709b3fe4a274af077e7a801ae2d4ef8a46
kaedejohnson <kaede.johnson@epfl.ch>
2023-11-29 20:30:09 +0100

    conf threshold

--- Clustering.py
+++ Clustering.py
@@ -39,11 +39,12 @@ def visualize_polygons(clustered, img_path):
     return image
 
 
-def cluster_polygons(p_labels):
+def cluster_polygons(p_labels, conf_threshold):
 
     features = []
     for i in range(len(p_labels['polygon_x'])):
-        features.append(get_feature(p_labels['polygon_x'][str(i)], p_labels['polygon_y'][str(i)]))
+        if p_labels['score'] > conf_threshold:
+            features.append(get_feature(p_labels['polygon_x'][str(i)], p_labels['polygon_y'][str(i)]))
 
     clustering = DBSCAN(eps=50, min_samples=3).fit(np.array(features))
     c_labels = clustering.labels_
@@ -75,7 +76,7 @@ def cluster_polygons(p_labels):
 
 with open('test/combined_tagged_all_layers.json', 'r', encoding='utf-8') as f:
 
-    clustered = cluster_polygons(json.load(f))
+    clustered = cluster_polygons(json.load(f), conf_threshold=.5)
 
     # visualize clusters
     image = visualize_polygons(clustered, 'test/combined.png')
