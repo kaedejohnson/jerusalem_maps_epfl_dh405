5e494d252d7b7c4bc25e89ec47834b87b3523824
SesamePaste233 <61174335+SesamePaste233@users.noreply.github.com>
2023-12-05 15:34:57 +0100

    PCA update

--- Grouping.py
+++ Grouping.py
@@ -6,23 +6,25 @@ from itertools import combinations
 def calc_PCA_feats(polygons, do_separation = True, enhance_coords = True):
     PCA_features = []
     for poly in polygons:
-        _coords = []
+        raw_coords = []
         if isinstance(poly, sh.geometry.polygon.Polygon):
             polygon_x = poly.exterior.coords.xy[0]
             polygon_y = poly.exterior.coords.xy[1]
             for x, y in zip(polygon_x, polygon_y):
-                _coords.append([x, y])
+                raw_coords.append([x, y])
         elif isinstance(poly, sh.geometry.multipolygon.MultiPolygon):
             for p in poly:
                 polygon_x = p.exterior.coords.xy[0]
                 polygon_y = p.exterior.coords.xy[1]
                 for x, y in zip(polygon_x, polygon_y):
-                    _coords.append([x, y])
+                    raw_coords.append([x, y])
         
-        coords = _coords
-        if enhance_coords == True:
-            for a, b in combinations(_coords, 2):
+        coords = raw_coords
+        if enhance_coords == True: # Try to sample in the whole polygon
+            for a, b in combinations(raw_coords, 2):
                 coords.append([(a[0] + b[0])/2.0, (a[1] + b[1])/2.0])
+                coords.append([(a[0] + 2*b[0])/3.0, (a[1] + 2*b[1])/3.0])
+                coords.append([(2*a[0] + b[0])/3.0, (2*a[1] + b[1])/3.0])
 
         _pca = PCA(n_components = 2)
         _pca.fit(coords)
@@ -38,13 +40,16 @@ def calc_PCA_feats(polygons, do_separation = True, enhance_coords = True):
                 PCA_features.append([{'Centroid': centroid,'PCA_Var': V, 'PCA_Basis': C}])
             else:
                 U = _pca.transform(coords)[:, 0]
-                A_coords = []
-                B_coords = []
+                raw_A_coords = []
+                raw_B_coords = []
                 for coord, u in zip(coords, U):
                     if u > 0:
-                        A_coords.append(coord)
+                        raw_A_coords.append(coord)
                     else:
-                        B_coords.append(coord)
+                        raw_B_coords.append(coord)
+
+                A_coords = raw_A_coords
+                B_coords = raw_B_coords
 
                 _pca_A = PCA(n_components = 2)
                 _pca_A.fit(A_coords)
