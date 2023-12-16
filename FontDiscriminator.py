406731e7811c0ea1f600b4185f02b28c53ba6048
tianhao <thdai2000@163.com>
2023-12-16 23:08:25 +0100

    update FontDiscriminator.py

--- FontDiscriminator.py
+++ FontDiscriminator.py
@@ -9,10 +9,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import random
+import cv2
+
 
 with open("dependencies/ground_truth_labels/ground_truth_labels.json", "r", encoding="utf-8") as f:
     data_list = json.load(f)["data"]
-
+    print(len(data_list))
 
 # Define a custom dataset
 class ImagePairDataset(Dataset):
@@ -23,14 +26,45 @@ class ImagePairDataset(Dataset):
     def __len__(self):
         return len(self.data_list)
 
+    def pre_processing(self, img_path):
+        # Load the image
+        image = cv2.imread(img_path)
+        # Convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Apply Gaussian blur to reduce noise
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        # Apply adaptive thresholding to get a binary image
+        thresh = cv2.adaptiveThreshold(
+            blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+        )
+        # Apply morphological operations to clean up small dots/noise and close gaps in text
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        # Resize image to 128x128 as per your requirement
+        resized = cv2.resize(morph, (128, 128), interpolation=cv2.INTER_AREA)
+        # Convert to tensor
+        tensor_image = transforms.ToTensor()(resized)
+
+        # # If you need to view the processed image:
+        # cv2.imshow("Processed Image", resized)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        return tensor_image
+
     def __getitem__(self, idx):
         img1_path, img2_path, label = self.data_list[idx]
-        img1 = Image.open(img1_path)
-        img2 = Image.open(img2_path)
-        if self.transform:
-            img1 = self.transform(img1)
-            img2 = self.transform(img2)
-        return img1, img2, torch.tensor(label, dtype=torch.float32)
+
+        # img1 = Image.open(img1_path)
+        # img2 = Image.open(img2_path)
+        # if self.transform:
+        #     img1 = self.transform(img1)
+        #     img2 = self.transform(img2)
+
+        img1 = self.pre_processing(img1_path)
+        img2 = self.pre_processing(img2_path)
+
+        return img1, img2, torch.tensor(label, dtype=torch.float32), img1_path, img2_path
 
 
 # Define the CNN architecture
@@ -62,6 +96,17 @@ class SimpleCNN(nn.Module):
         return torch.sigmoid(x)
 
 
+# Set a fixed seed for reproducibility
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # For multiGPU setups
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
 # Instantiate the model
 model = SimpleCNN()
 
@@ -83,53 +128,97 @@ train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 # Create DataLoaders for training and testing sets
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 # Check if CUDA is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-# Transfer the model to the GPU
-model.to(device)
+# # Transfer the model to the GPU
+# model.to(device)
+#
+# # Training loop
+# num_epochs = 80
+# for epoch in range(num_epochs):
+#     model.train()  # Set the model to training mode
+#     for img1, img2, labels in train_loader:
+#         img1, img2, labels = img1.to(device), img2.to(device), labels.to(device)
+#         outputs = model(img1, img2)
+#         outputs = outputs.squeeze()
+#         loss = criterion(outputs, labels)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+#
+#     if (epoch + 1) % 10 == 0:
+#         # Evaluation with Confusion Matrix
+#         model.eval()
+#         all_labels = []
+#         all_predictions = []
+#
+#         with torch.no_grad():
+#             for img1, img2, labels, _, _ in test_loader:
+#                 img1, img2, labels = img1.to(device), img2.to(device), labels.to(device)
+#                 outputs = model(img1, img2)
+#                 outputs = outputs.squeeze()
+#                 predicted = (outputs > 0.5).float()
+#
+#                 all_labels.extend(labels.cpu().numpy())
+#                 all_predictions.extend(predicted.cpu().numpy())
+#
+#         conf_matrix = confusion_matrix(all_labels, all_predictions)
+#         accu = np.trace(conf_matrix) / np.sum(conf_matrix)
+#         # Accuracy
+#         print("Accuracy:", accu)
+#         # Precision
+#         print("Precision:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[0, 1]))
+#         # Recall
+#         print("Recall:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[1, 0]))
+#         # Confusion Matrix
+#         print("Confusion Matrix:\n", conf_matrix)
+#         torch.save(model.state_dict(), f"font_disc_weights/FontDiscriminator_afterpad_{epoch + 1}_{accu * 100:.2f}.pth")
 
-# Training loop
-num_epochs = 80
-for epoch in range(num_epochs):
-    model.train()  # Set the model to training mode
-    for img1, img2, labels in train_loader:
+
+# evaluation
+model.load_state_dict(torch.load("font_disc_weights/FontDiscriminator_afterpad_40_96.96.pth"))
+model.to(device)
+model.eval()
+all_labels = []
+all_predictions = []
+wrong_pairs = []
+
+with torch.no_grad():
+    for img1, img2, labels, img1_path, img2_path in test_loader:
         img1, img2, labels = img1.to(device), img2.to(device), labels.to(device)
         outputs = model(img1, img2)
         outputs = outputs.squeeze()
-        loss = criterion(outputs, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+        predicted = (outputs > 0.5).float()
 
-    if (epoch + 1) % 10 == 0:
-        # Evaluation with Confusion Matrix
-        model.eval()
-        all_labels = []
-        all_predictions = []
+        # print(img1_path[0])
+        # print(img2_path[0])
+        # print(labels.item())
+        # print(predicted.item())
 
-        with torch.no_grad():
-            for img1, img2, labels in test_loader:
-                img1, img2, labels = img1.to(device), img2.to(device), labels.to(device)
-                outputs = model(img1, img2)
-                outputs = outputs.squeeze()
-                predicted = (outputs > 0.5).float()
+        all_labels.append(labels.item())
+        all_predictions.append(predicted.item())
 
-                all_labels.extend(labels.cpu().numpy())
-                all_predictions.extend(predicted.cpu().numpy())
+        if labels.item() != predicted.item():
+            wrong_pairs.append([img1_path[0], img2_path[0], int(labels.item())])
 
-        conf_matrix = confusion_matrix(all_labels, all_predictions)
-        accu = np.trace(conf_matrix) / np.sum(conf_matrix)
-        # Accuracy
-        print("Accuracy:", accu)
-        # Precision
-        print("Precision:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[0, 1]))
-        # Recall
-        print("Recall:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[1, 0]))
-        # Confusion Matrix
-        print("Confusion Matrix:\n", conf_matrix)
-        torch.save(model.state_dict(), f"font_disc_weights/FontDiscriminator_{epoch + 1}_{accu * 100:.2f}.pth")
+        # all_labels.extend(labels.cpu().numpy())
+        # all_predictions.extend(predicted.cpu().numpy())
+
+conf_matrix = confusion_matrix(all_labels, all_predictions)
+accu = np.trace(conf_matrix) / np.sum(conf_matrix)
+# Accuracy
+print("Accuracy:", accu)
+# Precision
+print("Precision:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[0, 1]))
+# Recall
+print("Recall:", conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[1, 0]))
+# Confusion Matrix
+print("Confusion Matrix:\n", conf_matrix)
+
+with open('wrong_pairs.json', 'w') as file:
+    json.dump({"data": wrong_pairs}, file)
